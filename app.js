ec100524c5d7abc97a05c42add455f7f6792f65b
mreed5812 <mreed5812@gmail.com>
2019-03-28 22:28:02 -0500

    added basic functions

---
+++ app.js
@@ -0,0 +1,21 @@
+function add(x,y){
+	return x + y;
+}
+
+function subtract(x,y){
+	return x - y;
+}
+
+function multiply(x,y){
+	return x * y;
+}
+
+function divide(x,y){
+	return x % y;
+}
+
+function operate(operator,a,b){
+	//do something with integers and operator (+,-,*,%)
+
+}
+console.log(add(54,66));
