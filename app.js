1c8026f753457e517ba9c09c40068e78cf4513ef
mreed5812 <mreed5812@gmail.com>
2019-04-04 06:34:16 -0500

    reworked functions and changed display to use textContent

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+
 function add(x,y){
 	return x + y;
 }
@@ -11,35 +12,89 @@ function multiply(x,y){
 }
 
 function divide(x,y){
-	return x % y;
+	return x / y;
 }
 
 function operate(operator,x,y){
 	switch(operator){
 		case 'multiply':
-			console.log(multiply(x,y));
+			return multiply(x,y);
 			break;
 
 		case 'add':
-			console.log(add(x,y));
+			return add(x,y);
 			break;
 
 		case 'subtract':
-			console.log(subtract(x,y));
+			return subtract(x,y);
 			break;
 
 		case 'divide':
-			console.log(divide(x,y));
+			return divide(x,y);
 			break;
 	}
 }	
 
-$('.button').on('click', function(event){
-	//get id of event target
-	let eventID = event.target.id;
+const display = document.querySelector("#output");
+const numbers = document.querySelectorAll(".number");
+const operators = document.querySelectorAll(".operator");
+const equalBtn = document.querySelector("#equals");
+const clearBtn = document.querySelector("#clear");	
+const decimalBtn = document.querySelector("#decimal");
+const deleteBtn = document.querySelector("#delete");
+const divideBtn = document.querySelector("#divide");
 
-	//determine what number/operator it is
-	alert(eventID);
-	//add to display
+let var1, var2, operator;
 
+//^ matches position just before first character of string
+// $ matches position just after last character of string
+// . matches a single character
+//* matches preceding item 0 or more times
+//? matches preceiding 0 or 1 time
+let numCheck = /^\d*\.?\d*$/;
+
+function clear(){
+	display.textContent = null;
+	var1 = null;
+	var2 = null;
+}
+
+deleteBtn.addEventListener('click', function(){
+	display.textContent = display.textContent.slice(0,-1);
+});
+
+clearBtn.addEventListener('click', function(){
+	clear();
+});
+
+equalBtn.addEventListener('click', function(){
+	var2 = parseFloat(display.textContent);
+	let result = operate(operator, var1,var2).toFixed(2);
+	display.textContent = result;
+});
+
+numbers.forEach((button) => {
+	button.addEventListener('click', (e) => {
+		if (display.textContent == '0') {
+			display.textContent = button.textContent;
+		} else if (numCheck.test(display.textContent)) {
+			display.textContent += button.textContent;
+		} else {
+			display.textContent = button.textContent;
+		}
+	});
+});
+
+operators.forEach((button) => {
+	button.addEventListener('click', (event) => {
+		var1 = parseFloat(display.textContent);
+		operator = event.target.id;
+		display.textContent = button.textContent;
+	});
+});
+
+decimalBtn.addEventListener('click', (event) => {
+	if (display.textContent.indexOf('.') < 1) {
+		display.textContent += '.';
+	}
 });
